f51ec85b20e3b3d68635377fc456cdc6d170205a
David Snopek <dsnopek@gmail.com>
2025-09-24 16:42:00 -0500

    A bunch of junk changes - not quite working

--- docker/build-rootfs.sh
+++ docker/build-rootfs.sh
@@ -4,17 +4,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ALPINE_ANDROID_DIR="$SCRIPT_DIR/../thirdparty/alpine-android"
 OUTPUT_DIR="$SCRIPT_DIR/../app/src/main/assets/linux-rootfs"
 
+IMAGE_NAME=godotengine/alpine-android:android-35-jdk17
+
 docker build "$ALPINE_ANDROID_DIR/docker" --platform=linux/arm64/v8 -f "$ALPINE_ANROID_DIR/docker/base.Dockerfile" -t alvrme/alpine-android-base:jdk17 \
 	--build-arg="JDK_VERSION=17" --build-arg="CMDLINE_VERSION=latest" --build-arg="SDK_TOOLS_VERSION=13114758"
 
 docker build "$ALPINE_ANDROID_DIR/docker" --platform=linux/arm64/v8 -f "$ALPINE_ANDROID_DIR/docker/android.Dockerfile" -t alvrme/alpine-android:android-35-jdk17 \
 	--build-arg="JDK_VERSION=17" --build-arg="BUILD_TOOLS=35.0.0" --build-arg="TARGET_SDK=35"
 
-docker build "$SCRIPT_DIR" --platform=linux/arm64/v8 -f "$SCRIPT_DIR/Dockerfile.godot" -t godotengine/alpine-android:android-35-jdk17 \
+docker build "$SCRIPT_DIR" --platform=linux/arm64/v8 -f "$SCRIPT_DIR/Dockerfile.godot" -t $IMAGE_NAME \
 	--build-arg="JDK_VERSION=17" --build-arg="BUILD_TOOLS=35.0.0" --build-arg="TARGET_SDK=35"
 
-CONTAINER_ID=$(docker create --platform=linux/arm64/v8 godotengine/alpine-android:android-35-jdk17)
+CONTAINER_ID=$(docker create --platform=linux/arm64/v8 $IMAGE_NAME)
 docker export --output="$OUTPUT_DIR/alpine-android-35-jdk17.tar" $CONTAINER_ID
+#docker inspect $IMAGE_NAME --format='{{json .Config.Env}}' | python -c 'import json, sys; d=json.load(sys.stdin); print(*d, sep="\n")' > "$OUTPUT_DIR/alpine-android-35-jdk17.env"
 docker rm $CONTAINER_ID
 
 rm -f "$OUTPUT_DIR/alpine-android-35-jdk17.tar.xz"
