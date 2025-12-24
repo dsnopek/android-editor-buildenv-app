5586bd11399610cddccc8c5b08bd65c8aa944bdf
David Snopek <dsnopek@gmail.com>
2025-12-24 07:04:52 -0600

    Write project info to the project cache directory

---
+++ app/src/main/java/org/godotengine/godot_gradle_build_environment/ProjectInfo.kt
@@ -0,0 +1,41 @@
+package org.godotengine.godot_gradle_build_environment
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+data class ProjectInfo(
+    val projectPath: String,
+    val gradleBuildDir: String
+) {
+    companion object {
+        private const val PROJECT_INFO_FILENAME = ".gabe_project_info.json"
+
+        private val json = Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+        }
+
+        fun writeToDirectory(directory: File, projectPath: String, gradleBuildDir: String) {
+            val projectInfo = ProjectInfo(projectPath, gradleBuildDir)
+            val jsonString = json.encodeToString(projectInfo)
+            val file = File(directory, PROJECT_INFO_FILENAME)
+            file.writeText(jsonString)
+        }
+
+        fun readFromDirectory(directory: File): ProjectInfo? {
+            val file = File(directory, PROJECT_INFO_FILENAME)
+            return if (file.exists()) {
+                try {
+                    json.decodeFromString<ProjectInfo>(file.readText())
+                } catch (e: Exception) {
+                    null
+                }
+            } else {
+                null
+            }
+        }
+    }
+}
