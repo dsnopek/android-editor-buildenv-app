2730d4d0836863938fdd2df006bf5383d9dd9fe5
David Snopek <dsnopek@gmail.com>
2025-12-24 06:11:25 -0600

    Added the scaffolding for a bottom menu

---
+++ app/src/main/java/org/godotengine/godot_gradle_build_environment/screens/SettingsScreen.kt
@@ -0,0 +1,32 @@
+package org.godotengine.godot_gradle_build_environment.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Settings",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Coming soon...",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
